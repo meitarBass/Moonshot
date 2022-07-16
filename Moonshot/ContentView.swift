89382a2bdc0b0d70d6a6acd4556786c6e19e49f4
meitarbasson <meitarbass@gmail.com>
2022-07-17 01:35:00 +0300

    part 5 day 39.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,28 +7,28 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
+//struct CustomText: View {
+//    let text: String
+//
+//    var body: some View {
+//        Text(text)
+//    }
+//
+//    init(_ text: String) {
+//        print("Creating a new CustomText")
+//        self.text = text
+//    }
+//}
+//
+//struct User: Codable {
+//    let name: String
+//    let address: Address
+//}
+//
+//struct Address: Codable {
+//    let street: String
+//    let city: String
+//}
 
 struct ContentView: View {
     var body: some View {
@@ -62,21 +62,33 @@ struct ContentView: View {
 //        }
         
         
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+//        Button("Decode JSON") {
+//            let input = """
+//            {
+//                "name": "Taylor Swift",
+//                "address": {
+//                    "street": "555, Taylor Swift Avenue",
+//                    "city": "Nashville"
+//                }
+//            }
+//            """
+//
+//            let data = Data(input.utf8)
+//            let decoder = JSONDecoder()
+//            if let user = try? decoder.decode(User.self, from: data) {
+//                print(user.address.street)
+//            }
+//        }
+        
+        let layout = [
+            GridItem(.adaptive(minimum: 80, maximum: 120)),
+        ]
+        
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
             }
         }
     }
