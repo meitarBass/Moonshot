ca47bf6f8f5f8d0ac2b1ebac186139cdaaf68f95
meitarbasson <meitarbass@gmail.com>
2022-07-19 01:49:00 +0300

    Finished day 41.

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,37 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Meitar Basson on 19/07/2022.
+//
+
+import SwiftUI
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
